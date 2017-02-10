2f3b44ab37ef27d0e4e620a252f60891fdfe4f53
Asad Sahi <asadazmat@gmail.com>
2017-02-10 19:48:50 +0000

    initial

---
+++ src/Pluralsight.Crypto/UI/CertDetailsForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography;
+
+namespace Pluralsight.Crypto.UI
+{
+    public partial class CertDetailsForm : Form
+    {
+        public StoreLocation CertStoreLocation { get; set; }
+        public StoreName CertStoreName { get; set; }
+        public X509Certificate2 Certificate { get; set; }
+
+        public CertDetailsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CertDetailsForm_Load(object sender, EventArgs e)
+        {
+            txtStore.Text = string.Format("{0}/{1}", CertStoreLocation, CertStoreName);
+            txtThumbprint.Text = Certificate.Thumbprint;
+
+            RSACryptoServiceProvider privateKey = Certificate.PrivateKey as RSACryptoServiceProvider;
+            if (null != privateKey)
+            {
+                string keyFile = privateKey.CspKeyContainerInfo.UniqueKeyContainerName;
+                txtPrivateKeyFile.Text = Path.Combine(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), @"Microsoft\Crypto\RSA\MachineKeys"), keyFile);
+            }
+            else btnViewPrivateKeyFile.Enabled = false;
+        }
+
+        private void btnViewStore_Click(object sender, EventArgs e)
+        {
+            X509Store store = new X509Store(CertStoreName, CertStoreLocation);
+            store.Open(OpenFlags.ReadOnly);
+
+            X509Certificate2UI.SelectFromCollection(store.Certificates, txtStore.Text, "", X509SelectionFlag.SingleSelection);
+        }
+
+        private void btnViewCert_Click(object sender, EventArgs e)
+        {
+            X509Certificate2UI.DisplayCertificate(Certificate);
+        }
+
+        private void btnViewPrivateKeyFile_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("explorer.exe", "/select," + txtPrivateKeyFile.Text);
+        }
+    }
+}
---
+++ src/SelfCert/Program.cs
@@ -0,0 +1,56 @@
+﻿//
+// This code was written by Keith Brown, and may be freely used.
+// Want to learn more about .NET? Visit pluralsight.com today!
+//
+using System;
+using Pluralsight.Crypto.UI;
+using System.Windows.Forms;
+using Pluralsight.Crypto;
+using System.Security.Cryptography.X509Certificates;
+
+namespace GenSelfSignedCert
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            new GenerateSelfSignedCertForm().ShowDialog();
+        }
+
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(((Exception)e.ExceptionObject).ToString());
+        }
+
+        // here's a simple example of how to gen a cert programmatically using Pluralsight.Crypto
+        // note you'll need to also reference System.Security.dll to get support for X509Certificate2UI.
+        static void GenSelfSignedCert()
+        {
+            using (CryptContext ctx = new CryptContext())
+            {
+                ctx.Open();
+
+                X509Certificate2 cert = ctx.CreateSelfSignedCertificate(
+                    new SelfSignedCertProperties
+                    {
+                        IsPrivateKeyExportable = true,
+                        KeyBitLength = 4096,
+                        Name = new X500DistinguishedName("cn=localhost"),
+                        ValidFrom = DateTime.Today.AddDays(-1),
+                        ValidTo = DateTime.Today.AddYears(1),
+                    });
+
+                X509Certificate2UI.DisplayCertificate(cert);
+            }
+        }
+    }
+}
